3f94cd618bdbc92e3dfc9377de2cc44a24c302cb
root <root@ticha.haverford.edu>
2016-02-16 11:36:53 -0500

    footerandheader

--- common/footer.php
+++ common/footer.php
@@ -1,6 +1,6 @@
         </article>
 
-        <footer id="footer">
+       <!-- <footer id="footer">
 
             <nav id="bottom-nav">
                 <?php echo public_nav_main(); ?>
@@ -20,7 +20,67 @@
 
             <?php fire_plugin_hook('public_footer', array('view'=>$this)); ?>
 
-        </footer>
+        </footer>-->
+<footer>		  
+  <div class="col-xs-12">
+                <p class = "blockcenter" style = "font-style: normal;">
+                       <h5>How to cite</h5>
+                           Lillehaugen, Brook Danielle, George Aaron Broadwell, Michel R. Oudijk, &amp; Laurie Allen. 2015. Ticha: a digital text explorer for Colonial Zapotec, first edition. Online: <a href="index.html">http://ticha.haverford.edu/</a></p>
+
+
+       </div>
+
+<br>
+		   
+			
+		<div class = "col-lg-2 col-md-2 col-sm-4 col-xs-4 text-center">
+		<p class = "text-center">
+		  		<a href="http://creativecommons.org/licenses/by-sa/4.0/">
+		  			<img src="http://i.creativecommons.org/l/by-sa/4.0/88x31.png" class="img-responsive" alt="This works is licensed under a Creative Commons Attribution-ShareAlike (CC BY-SA) license."/>
+		  		</a>
+				<p></p>
+			</div>
+			<div class = "col-lg-2 col-md-2 col-sm-4 col-xs-4 text-center">    
+				<a href="http://www.haverford.edu/">
+				<img src="<?php echo img('Haverford_Wordmark_Red_for_web.jpg'); ?>" class="img-responsive" alt="" width="170" height="42" />
+			    </a>
+				<p></p>
+			</div>
+			<div class = "col-lg-2 col-md-2 col-sm-4 col-xs-4 text-center">    
+				<a href="http://tdh.brynmawr.edu/">
+				<img src="<?php echo img('TricoDH-logo.jpg'); ?>" class="img-responsive" alt="This project generously supported with funding from the Tri-Co Digital Humanties." width="150" height="79" />
+				</a>
+				<p></p>
+			</div>
+			<div class = "col-lg-2 col-md-2 col-sm-4 col-xs-4 text-center">    
+				<a href="http://www.neh.gov/">
+				<img src="<?php echo img('neh_at_logo.png') ?>" style="margin-right: 50px" class="img-responsive" alt="All opinions, findings, conclusions, or recommendations expressed in this project do not necessarily represent those of the National Endowment for the Humanities." width="170" height="41" />
+				</a>
+				<p></p>
+			</div>
+			<div class = "col-lg-1 col-md-1 col-sm-2 col-xs-2 text-center">    
+              <a href="http://www.haverford.edu/HCAH/">
+				<img src="<?php echo img('hcahlogo.jpg'); ?>" class = "img-responsive" width="75" height="75" />
+			  </a>
+			<p></p>
+			</div>
+			<div class = "col-lg-2 col-md-2 col-sm-3 col-xs-3 text-center">    
+                <a href="http://www.acls.org/">
+                 <img src="<?php echo img('acls-logo.gif'); ?>" class = "img-responsive" width = "150" height="75">
+             	</a>
+			<p></p>
+			</div>
+			<div class = "col-lg-1 col-md-1 col-sm-3 col-xs-3 text-center">    
+			<p></p>
+			<br>
+				<p>
+				<a class = "text-center" href="#">Back to top</a>
+				</p>
+			</div>
+		
+			
+
+     </footer>
 
     </div><!-- end wrap -->
 </body>
--- common/header.php
+++ common/header.php
@@ -23,11 +23,15 @@
     <?php
     queue_css_url('http://fonts.googleapis.com/css?family=Oxygen');
     queue_css_url('http://fonts.googleapis.com/css?family=Inconsolata');
+    queue_css_url('http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css');
     queue_css_file(array(
-        'bootstrap',
-        'font-awesome',
+    //    'bootstrap',
+        'bootstrap_journal',
+	'font-awesome',
         'style',
-        'ol',
+	'ol',
+	'custom',
+	'small_nav',
     ));
     echo head_css();
     ?>
@@ -51,9 +55,104 @@
     $scripto = ScriptoPlugin::getScripto();
 ?>
 
-    <div id="wrap" class="container">
+<!--    <div id="wrap" class="container">-->
+<header>
+<nav class="navbar navbar-inverse" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
 
-        <header id="header" role="banner">
+                <!--<a class="navbar-brand" href="index_new.html">
+                    <img src="../img/logo2_long.png" alt="" height="120" width="380">
+                </a>-->
+               <!-- gets page url, places it after /es/ (in Spanish subdirectory) -->
+<a href="../es/" id="language-button" class="btn btn-default">
+<!-- Warning!: with subdirectories added, this url will stop working -->
+  Español
+</a>
+
+				<p><h2><a href="index.html">Ticha</a></h2></p>
+				<p><h4>a digital text explorer for Colonial Zapotec</h4></p>
+
+			</div>
+			<div class="navbar-collapse collapse navbar-ex1-collapse">
+              <ul class="nav navbar-nav navbar-right">
+                <!--<li class="active"><a href="index_new.html">Home</a></li>-->
+				<li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">About<b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="about.html">The project</a></li>
+					<li><a href="team.html">The team</a></li>
+					<li><a href="acknowledgements.html">Acknowledgements</a></li>
+                    <li><a href="form.html">Contact us</a></li>
+                  </ul>
+				  </li>
+				<li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Colonial Zapotec<b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="linguistic.html">Linguistic background</a></li>
+                    <li><a href="context.html">Cultural context</a></li>
+                  </ul>
+				</li>
+				<li class="dropdown">
+                  <a href="texts.html" class="dropdown-toggle" data-toggle="dropdown">Explore texts<b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li class="dropdown-header"><a href="texts.html">Explore texts</a></li>
+					<li class="divider"></li>
+                    <li class="dropdown-header"><a href="arte.html">Arte en lengua zapoteca</a></li>
+					<!--<li><a href="cordova.html">About Juan de Cordova</a></li>-->
+				            <li><a href="outline.html">Outline</a></li>
+                    <li><a href="arte_pdf.html">PDF</a></li>
+                    <li><a href="sample_arte.html">Sample page</a></li>
+                    <li><a href="arte_original.html">Transcription</a></li>
+                    <li><a href="reg_spanish.html">Regularized Spanish</a></li>
+					<li class="divider"></li>
+                    <li class="dropdown-header"><a href="doctrina.html">Doctrina christiana en lengua castellana y çapoteca</a></li>
+					          <li><a href="feria.html">About Pedro de Feria</a></li>
+                    <li><a href="doctrina_pdf.html">PDF</a></li>
+                    <li><a href="sample_doctrina.html">Sample page</a></li>
+					<li class="divider"></li>
+                    <li class="dropdown-header"><a href="handwritten.html">Handwritten texts</a></li>
+                    <li><a href="handwritten_texts.html">Available manuscripts</a></li>
+                    <li><a href="sample_handwritten.html">Sample manuscript</a></li>
+	                   <li><a href="timeline.html">Timeline of handwritten texts</a></li>
+                  </ul>
+				</li>
+				<!--<li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Resources<b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="bibliography.html">Bibliography</a></li>
+                    <li><a href="download.html">Download files</a></li>
+                  </ul>
+				</li> -->
+        <li><a href="bibliography.html">Bibliography</a></li>
+        <li>
+            <a href="https://www.facebook.com/TichaColonialZapotecTextExplorer" target="blank" class="btn btn-social-icon btn-facebook">
+                <i class="fa fa-facebook fb-follow"></i>
+            </a>
+        </li>
+        <li>
+             <!-- <a href="https://twitter.com/intent/tweet?button_hashtag=ticha" class="twitter-hashtag-button" data-url="http://ticha.haverford.edu">Tweet #ticha</a> -->
+              <a href="https://twitter.com/intent/tweet?button_hashtag=ticha&text=http://ticha.haverford.edu" class="btn btn-social-icon btn-twitter" data-url="http://ticha.haverford.edu">
+                <i class="fa fa-twitter"></i>
+            </a>
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
+        </li>
+            </ul> <!-- end navbar -->
+            </div><!-- end div class="navbar collapse" -->
+          </div>
+    </nav>
+
+</header>
+      <!--  <header id="header" role="banner">
 
             <div id="sublinks" class="masthead clearfix">
 
@@ -81,7 +180,7 @@
                 <a href="<?php echo WEB_ROOT; ?>"><img src="<?php echo img('sub.png'); ?>" alt="Scribe: an Omeka theme" title="Scribe: an Omeka theme" width="960" height="80" border="0"></a>
             </div>
 
-        </header><!-- end header -->
+        </header>--><!-- end header -->
 
         <article id="content">
 
