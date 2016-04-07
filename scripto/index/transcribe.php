80af0e3d34ab3f77f582263618a726dbd7e344b0
Laurie Allen <laallen@upenn.edu>
2016-04-07 14:29:52 -0400

    no titulo for filenames

--- scripto/index/transcribe.php
+++ scripto/index/transcribe.php
@@ -362,7 +362,7 @@ jQuery(document).ready(function() {
         <h3><?php echo $this->doc->getPageName(); ?></h3>
 
         <div>
-            <div><strong><?php echo metadata($this->file, array('Item Type Metadata', 'Título')); ?></strong></div>
+            <div><strong><?php echo metadata($this->file, array('Dublin Core', 'Title')); ?></strong></div>
             <div>image <?php echo html_escape($this->paginationUrls['current_page_number']); ?> of <?php echo html_escape($this->paginationUrls['number_of_pages']); ?></div>
             <div>
                 <?php //echo metadata($this->$file, array('Dublin Core', 'Source')); ?>
